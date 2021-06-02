78ef42b66d5e03528c030f0023793b48caa03cec
SilvioChoque <silviochoque02@gmail.com>
2021-06-02 14:54:30 -0300

    todos os operadores

--- Calculadora.php
+++ Calculadora.php
@@ -12,13 +12,17 @@
         $valor1 = $_GET["a"];
         $valor2 = $_GET["b"];
         $soma = $valor1 + $valor2;
-        echo "<br/>Primeiro numero da operação: $valor1";
-        echo "<br/>Segundo numero da operação: $valor2";
+        $subtracao = $valor1 - $valor2;
+        $multiplicacao = $valor1 * $valor2;
+        $divisao = $valor1 / $valor2;
+        $mod = $valor1 % $valor2;
+        echo "<br/>Primeiro numero da operação A: $valor1";
+        echo "<br/>Segundo numero da operação B: $valor2";
         echo "<br/>A soma dos dois numeros é: $soma";
-        
-
-
-
+        echo "<br/>A subtração da operação é: $subtracao ";
+        echo "<br/> Multiplicação da operação é: $multiplicacao";
+        echo "<br/> Divisão dos dois numros é: $divisao";
+        echo "<br/> Modulo dos dois numros é: $mod";
     ?>
 
     
