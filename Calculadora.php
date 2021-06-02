1c0e07cc11af94a52aaa352787a941c16e9bca0e
SilvioChoque <silviochoque02@gmail.com>
2021-06-02 07:43:18 -0300

    adicionando a página inicial

---
+++ Calculadora.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>iniciando o desenvolvimento da Calculadora </h1>
+    <?php
+        $valor1 = $_GET["a"];
+        $valor2 = $_GET["b"];
+        echo "Olá, Mundo! ";
+
+    ?>
+</body>
+</html>
